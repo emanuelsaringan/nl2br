72de15319b573406eb3dda12a90808ea39bc0e76
Marcus Pohorely <marcus@probots.io>
2021-04-20 12:15:38 +0200

    updated to string concatenation

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 module.exports = (str, isXhtml = false) => {
 	const breakTag = isXhtml ? '<br />' : '<br>';
-	return String(str).replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+	return String(str).replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, `$1${breakTag}$2`);
 };
